1a8936e38e5a57c499e00e3a9b684b0bd2df1fbd
domoyna <douomouo@gmail.com>
2024-01-09 17:48:27 +0300

    api key updated

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+function searchCity(city) {
+let apiKey = "5bd9to1c7af4e6e64c1939040db1b36a"
+let apiUrl =  "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}"
+}
+ 
 function handleSearchSubmit (event) {
     event.preventDefault ();
     let searchInput = document.querySelector("#search-form-input");
