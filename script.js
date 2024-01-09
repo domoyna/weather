4e6629967d67f946bcad21b98630b870df5367d4
domoyna <douomouo@gmail.com>
2024-01-09 17:15:15 +0300

    innerHtml updated

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function handleSearchSubmit(event) {
      event.preventDefault();
 let searchInput = document.querySelector("#search-form-input");
-let cityElement = document.querySelector("weather-app-city");
-cityElement=searchInput.value;
+let cityElement = document.querySelector("#city");
+
 cityElement.innerHTML = searchInput.value;
 
 }
