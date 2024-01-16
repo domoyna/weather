d406fc6881aecef03e4c31924b13813d61453f7b
domoyna <douomouo@gmail.com>
2024-01-16 15:18:44 +0300

    updated the humidity and description

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ let temperature = response.data.temperature.current;
  let descriptionElement = document.querySelector("#description");
  let humidityElement = document.querySelector("#humidity");
  let windSpeedElement = document.querySelector("#wind-speed");
-cityElement.innerHTML = response.data.city;
+
 descriptionElement.innerHTML=response.data.condition.description ;
 humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 windSpeedElement.innerHTML = `${response.data.wind.speed}km/h';
+cityElement.innerHTML = response.data.city;
 temperatureElement.innerHTML = Math.round(temperature);
 }
 
