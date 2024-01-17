5bb030874d3cee2b31c6b3220fc045ec21fcb802
domoyna <douomouo@gmail.com>
2024-01-17 15:03:26 +0300

    date hours minutes created as variable

--- script.js
+++ script.js
@@ -12,10 +12,25 @@ function updateWeather(response) {
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = `${response.data.main.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
-  timeElement.innerHTML = `${date.getHours()} : ${date.getMinutes()}`;
+  timeElement.innerHTML = `${date.getDay(} ${date.getHours()} : ${date.getMinutes()}`;
 
 }
-
+function formateDate (date) {
+  
+  let minutes = date.getMinutes;
+  let hours = date.getHours;
+let days = [
+  "Sunday",
+   "Monday",
+   "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"]
+]
+let day = days[getDay()];
+  return `${day} ${hours} : ${minutes}`;
+}
 function searchCity(city) {
   let apiKey = "515c9ddbeb3cda9061acfab71031839e";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(city)}&appid=${apiKey}&units=metric`;
@@ -30,6 +45,7 @@ function handleSearchSubmit(event) {
 
 
 }
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
