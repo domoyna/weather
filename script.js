be163f66a0ebe072522114279415d5d87ba1747b
domoyna <douomouo@gmail.com>
2024-01-21 20:55:01 +0300

    updated paris

--- script.js
+++ script.js
@@ -50,11 +50,10 @@ function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   searchCity(searchInput.value);
-
+searchCity "Paris";
 
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity "Paris";
