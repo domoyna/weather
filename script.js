b52bea8993a489827047ce3e46be6d8889d9e24a
domoyna <douomouo@gmail.com>
2024-01-23 04:26:16 +0300

    paeis by default

--- script.js
+++ script.js
@@ -57,3 +57,5 @@ function handleSearchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
